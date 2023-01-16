0e783ad57bc457cd143111cd21e6b41bb4cc4b86
Laurent Bugnion <LBugnion@outlook.com>
2023-01-16 10:17:32 +0100

    Add project files.

---
+++ TestExif/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Text;
+
+using (var pictureStream = File.Open(@"C:\Users\lbugn\Desktop\el20221202018D.jpg", FileMode.Open))
+{
+    var imageMetadata = Image.FromStream(pictureStream, false, false);
+
+    foreach (var item in imageMetadata.PropertyItems)
+    {
+        if (item.Value == null)
+        {
+            continue;
+        }
+
+        var encoding = new ASCIIEncoding();
+        var text = encoding.GetString(
+            item.Value,
+            0,
+            item.Len - 1);
+
+        Console.WriteLine($"0x{item.Id}: {text}");
+    }
+}
